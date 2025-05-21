4b23ed524433432cb65ae9284979956b1a433eab
Todd Densmore <469399+tdensmore@users.noreply.github.com>
2025-05-21 14:27:23 -0400

    Solution for env files in compose. Removed code from solution

--- 2-test/solution/backend-springboot/src/main/java/com/example/studentmanagement/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.studentmanagement.repository;
-
-import com.example.studentmanagement.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
